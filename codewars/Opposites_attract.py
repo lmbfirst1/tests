66213056409d34efae9b30d2ade8f9fd1ce00451
lmbfirst1 <sonniymoderator@gmail.com>
2023-05-05 13:54:05 +0400

    flowers

---
+++ codewars/Opposites_attract.py
@@ -0,0 +1,9 @@
+"""
+https://www.codewars.com/kata/555086d53eac039a2a000083/train/python
+"""
+
+def lovefunc( flower1, flower2 ):
+    if (flower1 + flower2) % 2 == 0:
+        return False
+    else:
+        return True
