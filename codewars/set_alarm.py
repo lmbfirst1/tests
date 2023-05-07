c1ba76202c9239bc6d36f1d983f7ca1e90f93276
lmbfirst1 <sonniymoderator@gmail.com>
2023-05-07 08:46:28 +0400

    set_alarm

---
+++ codewars/set_alarm.py
@@ -0,0 +1,6 @@
+"""
+https://www.codewars.com/kata/568dcc3c7f12767a62000038/train/python
+"""
+def set_alarm(employed, vacation):
+    return True if employed == True and vacation == False else False 
+
