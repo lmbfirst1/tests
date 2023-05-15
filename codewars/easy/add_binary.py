567a4a1736a24877943ec5b087a0504f3e168c7b
lmbfirst1 <sonniymoderator@gmail.com>
2023-05-15 14:19:10 +0400

    bin

---
+++ codewars/easy/add_binary.py
@@ -0,0 +1,11 @@
+"""
+https://www.codewars.com/kata/551f37452ff852b7bd000139/train/python
+Implement a function that adds two numbers together and returns their sum in binary. The conversion can be done before, or after the addition.
+
+The binary number returned should be a string.
+
+"""
+
+def add_binary(a,b):
+    c = a + b
+    return bin(c)[2:]
