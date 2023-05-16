c50b27b7820307478f9253a10a356f3932201364
lmbfirst1 <sonniymoderator@gmail.com>
2023-05-16 17:44:50 +0400

    is_triangle

---
+++ codewars/easy/is_triangle.py
@@ -0,0 +1,10 @@
+"""
+https://www.codewars.com/kata/56606694ec01347ce800001b/python
+Implement a function that accepts 3 integer values a, b, c. The function should return true if a triangle can be built with the sides of given length and false in any other case.
+
+(In this case, all triangles must have surface greater than 0 to be accepted).
+"""
+
+
+def is_triangle(a, b, c):
+    return (a<b+c) and (b<a+c) and (c<a+b)
