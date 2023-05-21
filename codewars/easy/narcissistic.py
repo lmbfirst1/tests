7dfd8a0bcd726aaf7c98d5dd428fd0abc08d0625
lmbfirst1 <sonniymoderator@gmail.com>
2023-05-21 12:12:37 +0400

    narcissistic

---
+++ codewars/easy/narcissistic.py
@@ -0,0 +1,16 @@
+"""
+https://www.codewars.com/kata/5287e858c6b5a9678200083c/train/python
+необходимо написать функцию, возвращающую True если число 
+является нарциссическим
+"""
+
+
+def narcissistic(value):
+    str_value = str(value)
+    numbers = len(str_value)
+    return (value == sum(int(digit) ** numbers for digit in str_value))
+
+
+value = 371
+narc_format = narcissistic(value)
+print(narc_format)
