83589518e0f272aa20abb35e03cf7a63acf59c4b
lmbfirst1 <sonniymoderator@gmail.com>
2023-05-22 17:57:32 +0400

    two_sum

--- codewars/medium/two_sum.py
+++ codewars/medium/two_sum.py
@@ -5,11 +5,14 @@ https://www.codewars.com/kata/52c31f8e6605bcc646000082/train/python
 """
 
 def two_sum(numbers, target):
-	true_indexes = []
-	extra_element = sum(numbers) - target
-	for i in numbers:
-		if i != extra_element:
-			index = numbers.index(i) 
-			true_indexes.append(index)
-		
-	return true_indexes
+    for i in range(len(numbers)):
+        for j in range(i + 1, len(numbers)):
+            if numbers[i] + numbers[j] == target:
+                return [i, j]
+
+
+
+
+# test.assert_equals(sorted(two_sum([1,2,3], 4)), [0,2])
+# test.assert_equals(sorted(two_sum([1234,5678,9012], 14690)), [1,2])
+# test.assert_equals(sorted(two_sum([2,2,3], 4)), [0,1])
