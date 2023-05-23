114165a343edc8e5cfd4d8a202a99dfc7233ce33
lmbfirst1 <sonniymoderator@gmail.com>
2023-05-23 10:23:05 +0400

    ref

--- codewars/medium/two_sum.py
+++ codewars/medium/two_sum.py
@@ -9,10 +9,10 @@ def two_sum(numbers, t):
     for i, x in enumerate(numbers):
         for j, y in enumerate(numbers):
             if i != j and x + y == t:
-                print([i, j])
+                # print([i, j])
                 return [i, j]
 
 
-two_sum([1, 2, 3], 4)
-two_sum([1234, 5678, 9012], 14690)
-two_sum([2, 2, 3], 4)
+# two_sum([1, 2, 3], 4)
+# two_sum([1234, 5678, 9012], 14690)
+# two_sum([2, 2, 3], 4)
