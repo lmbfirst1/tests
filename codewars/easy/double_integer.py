8da7cb634c2d0749fdd74536368f325933fa4429
lmbfirst1 <sonniymoderator@gmail.com>
2023-06-15 16:45:18 +0400

    work

---
+++ codewars/easy/double_integer.py
@@ -0,0 +1,9 @@
+"""
+https://www.codewars.com/kata/53ee5429ba190077850011d4/train/python
+Code as fast as you can! 
+You need to double the integer and return it.
+"""
+
+
+def double_integer(i):
+    return i * 2
